39c55a4d24b124e14b066e5153bdfc7abea6755f
Smith Krengkrud <smith.kre@gmail.com>
2017-05-30 11:18:56 +0700

    Edited setup file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Smith Krengkrud',
   author_email = 'smith.kre@gmail.com',
   url = 'https://github.com/smithkre/django-lazy-settings',
-  download_url = 'github.com/smithkre/django-lazy-settings/archive/1.0.tar.gz',
+  download_url = 'https://github.com/smithkre/django-lazy-settings/archive/1.0.tar.gz',
   keywords = ['django', 'settings'],
   classifiers = [],
 )
